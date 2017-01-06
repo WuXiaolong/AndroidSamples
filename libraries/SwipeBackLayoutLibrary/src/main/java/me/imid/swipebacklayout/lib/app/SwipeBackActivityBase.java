f8a835199f2d1e285cda4a7a24ef68e052fec528
WuXiaolong <wuxiaolong.me@gmail.com>
2017-01-06 10:53:23 +0800

    add

---
+++ libraries/SwipeBackLayoutLibrary/src/main/java/me/imid/swipebacklayout/lib/app/SwipeBackActivityBase.java
@@ -0,0 +1,21 @@
+package me.imid.swipebacklayout.lib.app;
+
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+
+/**
+ * @author Yrom
+ */
+public interface SwipeBackActivityBase {
+    /**
+     * @return the SwipeBackLayout associated with this activity.
+     */
+    SwipeBackLayout getSwipeBackLayout();
+
+    void setSwipeBackEnable(boolean enable);
+
+    /**
+     * Scroll out contentView and finish the activity
+     */
+    void scrollToFinishActivity();
+
+}
