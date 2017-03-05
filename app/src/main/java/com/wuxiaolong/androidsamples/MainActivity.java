ef9ad87f261b07eeb6fdcb0daa4d2aa25dbbb346
wuxiaolong.me <wuxiaolong.me@gmail.com>
2017-03-05 21:22:32 +0800

    RuntimePermission

--- app/src/main/java/com/wuxiaolong/androidsamples/MainActivity.java
+++ app/src/main/java/com/wuxiaolong/androidsamples/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
         //设置是否滑动退出
         setSwipeBackEnable(false);
